aac25c18265592a96476f1413cd9d29a4f1f7a14
ethan_huang <ethan_huang@sanwa-eng.com.tw>
2018-03-15 18:07:37 +0800

    auto mode added

---
+++ robotTest/Base/Slot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotTest.Base
+{
+    public class Slot
+    {
+        public string SlotNo { get; set; }
+        public string JobID { get; set; }
+        public string Status { get; set; }
+    }
+}
